c8ea4ab481a13aabef10704a74e8e0d83886520a
Chance03-git <Chancefoster05@gmail.com>
2024-11-26 23:06:51 -0500

    function for  generate normal array defined

--- ShapeArr.py
+++ ShapeArr.py
@@ -1,4 +1,9 @@
 import numpy as np
+def generate_normal_array(shape, mean, std_dev):
+    if std_dev < 0:
+        raise ValueError("Standard deviation cannot be negative.")
+
+    return np.random.normal(loc=mean, scale=std_dev, size=shape)
 def test_should_generate_values_within_distribution():
     """Tests that the generated values follow the specified mean and standard deviation."""
     shape = (1000,)
